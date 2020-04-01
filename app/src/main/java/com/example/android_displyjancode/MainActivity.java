486946b334388db54b9fbcfd77f239cd917bbce7
ssc-shimaoka <yuki_shimaoka@solver.co.jp>
2020-04-01 12:29:56 +0900

    zxing-android-embeddedライブラリ対応

--- app/src/main/java/com/example/android_displyjancode/MainActivity.java
+++ app/src/main/java/com/example/android_displyjancode/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android_displyjancode;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -17,15 +18,6 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.WriterException;
-import com.google.zxing.client.j2se.MatrixToImageWriter;
-import com.google.zxing.common.BitMatrix;
-import com.google.zxing.oned.EAN13Writer;
-
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -63,33 +55,5 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
-    }
-
-    public static void DisplayBarcode(String targetData, int width, int height)
-    {
-        int[] pixels = null;
-        try
-        {
-            BitMatrix bitData  = null;
-
-            EAN13Writer writer = new EAN13Writer();
-
-            //EAN13Writer#encode()には以下の情報を渡す
-            // (1)エンコード対象の文字列、バーコードシンボルに埋め込みたい情報
-            // (2)出力するバーコードの書式
-            // (3)イメージの幅
-            // (4)イメージの高さ
-            bitData  = writer.encode(targetData, BarcodeFormat.EAN_13, width, height);
-            //エンコードで得られたイメージを画像ファイルに出力する
-            FileOutputStream output = new FileOutputStream("mycode.png");
-            MatrixToImageWriter.writeToStream(bitData, "png", output);
-
-        }
-        catch (WriterException | FileNotFoundException e)
-        {
-            Log.e("ERROR","例外発生" + e);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
--- app/src/main/java/com/example/android_displyjancode/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/android_displyjancode/ui/home/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.example.android_displyjancode.ui.home;
 
 import android.arch.lifecycle.ViewModelProviders;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
@@ -13,17 +14,10 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.example.android_displyjancode.MainActivity;
 import com.example.android_displyjancode.R;
-
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.WriterException;
-import com.google.zxing.common.BitMatrix;
-import com.google.zxing.oned.EAN13Writer;
-
-
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 
 public class HomeFragment extends Fragment {
@@ -32,7 +26,7 @@ public class HomeFragment extends Fragment {
 
     // バーコードの各種設定
     String targetData = "";       //バーコードに変換する対象データ
-    int    width      = 400;      //作成するバーコードの幅
+    int    width      = 600;      //作成するバーコードの幅
     int    height     = 200;      //作成するバーコードの高さ
 
     public View root = null;
@@ -67,7 +61,7 @@ public class HomeFragment extends Fragment {
                     targetData = editText.getText().toString();
 
                     // バーコード表示処理
-                    MainActivity.DisplayBarcode(targetData,width,height);
+                    DisplayBarcode();
 
                     // バーコード表示を表示状態にする。
                     imageView.setVisibility(View.VISIBLE);
@@ -105,31 +99,21 @@ public class HomeFragment extends Fragment {
         });
     }
 
-    /*
     public void DisplayBarcode()
     {
-        int[] pixels = null;
+        ImageView imageView = root.findViewById(R.id.imageView);
         try
         {
-            BitMatrix bitData  = null;
+            targetData = "1234567890128"; //出バックコード
 
-            EAN13Writer writer = new EAN13Writer();
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
 
-            //EAN13Writer#encode()には以下の情報を渡す
-            // (1)エンコード対象の文字列、バーコードシンボルに埋め込みたい情報
-            // (2)出力するバーコードの書式
-            // (3)イメージの幅
-            // (4)イメージの高さ
-            bitData  = writer.encode(targetData, BarcodeFormat.EAN_13, width, height);
-            //エンコードで得られたイメージを画像ファイルに出力する
-            FileOutputStream output = new FileOutputStream("mycode.png");
-            MatrixToImageWriter.writeToStream(bitData, "png", output);
-
+            Bitmap bitmap = barcodeEncoder.encodeBitmap(targetData, BarcodeFormat.EAN_13, width, height);
+            imageView.setImageBitmap(bitmap);
         }
-        catch (WriterException | FileNotFoundException e)
+        catch (WriterException e)
         {
             Log.e("ERROR","例外発生" + e);
         }
     }
-     */
 }
